6f73692263d804a8c30236b3d1f630c49377441b
zianfranxes <franxeszian@gmail.com>
2024-10-26 20:05:51 +0200

    Finalizado

--- src/Ejercicio_07.java
+++ src/Ejercicio_07.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 public class Ejercicio_07 {
     public static void main(String[] args) {
 
+        //Inicio de Flujo de Trabajo
         Scanner scan = new Scanner(System.in);
 
+        //Declaracion de Variables
         int divisor = 2;
         int valorPrimo = 1;
 
+        //Recoleccion de Datos
         System.out.println("Inrtroduce un numero: ");
         int numero = scan.nextInt();
 
+        //Operacion Algoritmica
         while(divisor * divisor <= numero){
             if (numero  % divisor == 0){
                 valorPrimo = 0;
@@ -18,9 +22,11 @@ public class Ejercicio_07 {
             divisor++;
         }
 
+        //Si es primo lo dice
         if (valorPrimo == 1 && numero > 1){
             System.out.println(numero + " es un numero primo");
         }
+        //Si no es primo lo dice
         else {
             System.out.println(numero + " no es un numero primo");
         }
